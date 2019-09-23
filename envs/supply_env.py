b8d4a1fea27cae858cd84ce1a1dc45126c164cab
Jairo Jr. Rangel Redondo <jairotunior@gmail.com>
2019-09-22 22:59:50 -0500

    New Features

--- envs/supply_env.py
+++ envs/supply_env.py
@@ -2,7 +2,7 @@ import gym
 import gym.spaces
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 from gym.utils import seeding
 
 
@@ -13,30 +13,103 @@ class SupplyEnv(gym.Env):
 
     def __init__(self, start_date, end_date):
 
-        self.start_date = datetime.strptime(start_date, format="%Y/%m/%d")
-        self.end_date = datetime.strptime(end_date, format="%Y/%m/%d")
+        self.start_date = datetime.strptime(start_date, "%Y/%m/%d")
+        self.end_date = datetime.strptime(end_date, "%Y/%m/%d")
+
+        self.lead_time = timedelta(days=10)
 
         self.range_date = pd.date_range(start=start_date, end=end_date, freq='D')
 
-        self.current_date = self.range_date[0]
 
-        self.actions = ['pedir', 'no pedir', 'gerenciar']
+        self.orders = pd.DataFrame({'index': self.range_date,
+                                    'pedido': np.zeros((len(self.range_date),)),
+                                    'entrega': np.zeros((len(self.range_date),))
+                                    })
+        self.orders = self.orders.set_index('index')
+
+        self.history = pd.DataFrame({'index': self.range_date,
+                                    'consumo': np.zeros((len(self.range_date),)),
+                                    'stock': np.zeros((len(self.range_date),)),
+                                    'transito': np.zeros((len(self.range_date),)),
+                                    'disponible': np.zeros((len(self.range_date),)),
+                                    'pedido': np.zeros((len(self.range_date),))
+                                    })
+        self.history = self.history.set_index('index')
+
+        self.iterator = 0
+        self.current_date = self.range_date[self.iterator]
+
+        #self.actions = ['pedir', 'no pedir']
         
+        """
         self.action_space = gym.spaces.Tuple(
             (gym.spaces.Discrete(n=len(self.actions)), 
             gym.spaces.Box(low=1, high=np.inf, shape=(1,), dtype=np.int))
         )
-        self.observtion_space = gym.spaces.Box(low=0, high=np.inf, shape=(10, 1), dtype=np.int)
+        """
 
+        self.action_space = gym.spaces.Box(low=0, high=10000, shape=(1,), dtype=np.int)
+        self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(10, 1), dtype=np.int)
 
         self.seed()
 
+        self._calculate()
+
+
+    def _calculate(self):
+        self.history['consumo'] = np.random.randint(0, 
+                                            2000,
+                                            size=(len(self.history.index),)
+                                            )
+
+        # Initial Stock
+        self.history.at[self.current_date, 'stock'] = 2000
+
 
     def step(self, action):
-        pass
+        #if type(action) is np.ndarray:
+        #    print("Entero")
+
+        # Save the order
+        self.orders.at[self.current_date, 'pedido'] = action[0]
+        self.orders.at[self.current_date, 'entrega'] = self.range_date[self.iterator] + self.lead_time
+
+        self.history.at[self.current_date, 'pedido'] = action[0]
+
+        # Get incoming orders
+        mask = self.orders['entrega'] == self.current_date
+        incoming_orders = self.orders.loc[mask]
+        current_transito = 0
+
+        # If there are incoming orders, proceed to receive
+        if len(incoming_orders.index) > 0:
+            current_transito = incoming_orders['pedido'].sum()
+            self.history.at[self.current_date, 'transito'] = current_transito
+
+        # Get the current stock and consumo
+        current_stock = self.history.at[self.current_date, 'stock']
+        current_consumo = self.history.at[self.current_date, 'consumo']
+
+        # Validate if the environment ends
+        done = self.iterator == (len(self.range_date) - 1)
+
+        if not done:
+            # Update iterator and Current Date
+            self.iterator += 1
+            self.current_date = self.range_date[self.iterator]
+
+            # Set new stock for next day
+            self.history.at[self.current_date, 'stock'] = current_stock - current_consumo
+
+        obs, reward, _ = [None, 0, None]
+
+        return obs, reward, done, _
+
+
 
     def reset(self):
-        pass
+        self.iterator = 0
+        self.current_date = self.range_date[self.iterator]
 
     def close(self):
         pass
--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import gym
 from envs import SupplyEnv
 
-env = SupplyEnv()
+start_date = "2017/01/01"
+end_date = "2017/12/31"
+
+
+env = SupplyEnv(start_date=start_date, end_date=end_date)
 
 obs = env.reset()
+
+
+done = False
+
+while not done:
+    pedido = env.sample()
+
+    obs, reward, done, _ = env.step(pedido)
+
+    if done:
+        break
+
+print("Environment End")
+print(env.history)
+
+
 
